7f42c8f87e10169a99d7836fdcdca40b8d397243
Utk14007 <94047015+Utk14007@users.noreply.github.com>
2023-02-06 09:50:39 +0530

    Update utk.java

--- utk.java
+++ utk.java
@@ -1,6 +1,7 @@
 class CalcLength {
     public static void main( String args[] ) {
-       String name = "educative";  //Initilizing a String Object name
-       int length = name.length();  //Calling the inbuilt lenght() method
-       System.out.println("The length of the String \""+name+"\" is: " +length
+       String name = "educative"; 
+       int length = name.length(); 
+       System.out.println("The length of the String \""+name+"\" is: " +length);
+}
 }
